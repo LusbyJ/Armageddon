9bfd96053c01dab4491e95f8fab3cd5d386f782f
x2115 <henryhaak25@gmail.com>
2022-02-08 17:44:14 -0700

    Added Test Scene
    Added the test scene for the graybox.
    Updated the follow player script such that it clamped at the edges of the level and lerped smoothly
    Updated the main theme.

--- Assets/Scripts/Follow_player.cs
+++ Assets/Scripts/Follow_player.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Follow_player : MonoBehaviour {
 
     public Transform player;
-
+    public Vector2 minClamp= new Vector2(-999999,-999999);
+    public Vector2 maxClamp= new Vector2(999999,999999);
+    public float lerpSpeed=0.1f;
+    void Start (){
+      var position=player.transform.position+new Vector3(0,0,-5);
+      position.x=Mathf.Clamp(position.x,minClamp.x,maxClamp.x);
+      position.y=Mathf.Clamp(position.y,minClamp.y,maxClamp.y);
+      transform.position = position;
+    }
     // Update is called once per frame
-    void Update () {
-        transform.position = player.transform.position + new Vector3(0, 0, -5);
+    void FixedUpdate () {
+        var position=player.transform.position+new Vector3(0,0,-5);
+        position.x=Mathf.Clamp(position.x,minClamp.x,maxClamp.x);
+        position.y=Mathf.Clamp(position.y,minClamp.y,maxClamp.y);
+        transform.position = Vector3.Lerp(transform.position,position,lerpSpeed);
     }
 }
