bdd67f6bfce04b91bfa854cc152923e8c1a611fa
LusbyJ <jlusby13@gmail.com>
2022-02-17 12:48:09 -0700

    arm bug fixes

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,14 +6,16 @@ public class Bullet : MonoBehaviour
 {
     public int damage;
     public Rigidbody2D rb;
+    
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
         Enemy enemy = hitInfo.GetComponent<Enemy>();
         if(enemy != null)
-        {
+        {   
+            
             enemy.TakeDamage(damage);
-            Destroy(gameObject);
+    
         }
        
         
