bb7b1fe41cf0ecc185a97cf60d4a35123b554060
x2115 <henryhaak25@gmail.com>
2022-02-18 09:01:04 -0700

    Camera and UI
    Made some changes to the camera such that it slightly follows the mouse (will investigate how to increase this later, ran into bugs) and animated the UI so players can see which arm is active.

--- Assets/Scripts/HudController.cs
+++ Assets/Scripts/HudController.cs
@@ -18,17 +18,23 @@ public class HudController : MonoBehaviour
     private Weapon arm2;
     private Image MainIcon;
     private Image BackIcon;
+    private targetLerp mainPos;
+    private targetLerp backPos;
+    private Vector2 armActivePos=new Vector2(94,272);
+    private Vector2 armInactivePos=new Vector2(85,280);
     // Start is called before the first frame update
     void Start()
     {
       MainIcon=hudArmMainIcon.GetComponent<Image>();
       BackIcon=hudArmBackIcon.GetComponent<Image>();
+      mainPos=hudArmMain.GetComponent<targetLerp>();
+      backPos=hudArmBack.GetComponent<targetLerp>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Right Side Arm
         if (character.GetComponent<PickUp>().item1 != null)
         {
             hudArmMain.SetActive(true);
@@ -36,17 +42,33 @@ public class HudController : MonoBehaviour
             float perc = Mathf.Clamp((float)arm1.integrity / (float)arm1.maxIntegrity, 0, 1);
             hudArmBarMain.transform.localScale = new Vector3(perc, 1, 1);
             MainIcon.sprite=arm1.weaponIcon;
+            //Move the UI arm if it's active
+            if(arm1.holding==1){
+              mainPos.target=new Vector2(armActivePos.x,armActivePos.y);
+            }else{
+              mainPos.target=new Vector2(armInactivePos.x,armInactivePos.y);
+            }
         }
         else
         {
             hudArmMain.SetActive(false);
         }
+        //Left Side Arm
         if (character.GetComponent<PickUp>().item2 != null)
         {
             hudArmBack.SetActive(true);
             arm2 = character.GetComponent<PickUp>().item2.GetComponent<Weapon>();
+            //scale healthbar
             float perc = Mathf.Clamp((float)arm2.integrity / (float)arm2.maxIntegrity, 0, 1);
             hudArmBarBack.transform.localScale = new Vector3(perc, 1, 1);
+            //change icon sprite
+            BackIcon.sprite=arm2.weaponIcon;
+            //Move the UI arm if it's active
+            if(arm2.holding==1){
+              backPos.target=new Vector2(-armActivePos.x,armActivePos.y);
+            }else{
+              backPos.target=new Vector2(-armInactivePos.x,armInactivePos.y);
+            }
         }
         else
         {
