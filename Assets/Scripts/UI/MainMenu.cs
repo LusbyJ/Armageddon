129b0c8ee3a220cbe09da7f4e5b17b1396a8d272
x2115 <henryhaak25@gmail.com>
2022-02-18 21:12:48 -0700

    Main Menu
    Added a main menu scene

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public string playScene;
+    public void startGame(){
+      SceneManager.LoadScene(playScene);
+    }
+    public void endGame(){
+
+    }
+}
