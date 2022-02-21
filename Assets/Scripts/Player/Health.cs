e5c3776b26a6f479fa8bf9dd033fa8e12592c438
x2115 <henryhaak25@gmail.com>
2022-02-20 20:32:05 -0700

    Die=Game Over

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
@@ -51,7 +52,7 @@ public class Health : MonoBehaviour
 
     void Die()
     {
-        Destroy(gameObject);
+        SceneManager.LoadScene("GameOver");
     }
 
 }
