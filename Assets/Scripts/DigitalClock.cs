51ec2809346b9d9e93458dc030825df10b77a442
alopez57 <alopez57@cs.unm.edu>
2022-02-20 22:21:41 -0700

    Fixed Clock so it works like a stopwatch

--- Assets/Scripts/DigitalClock.cs
+++ Assets/Scripts/DigitalClock.cs
@@ -6,27 +6,37 @@ using System;
 
 public class DigitalClock : MonoBehaviour
 {
+    bool gameover = false;
     private Text textClock;
-
+    float currentTime;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         textClock = GetComponent<Text>();
+        currentTime = 0;
+
+        InvokeRepeating("CheckGameover", 0f, .5f);
+    }
+
+    void CheckGameover()
+    {
+        if (player.GetComponent<Health>().health == 0) 
+            gameover = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        DateTime time = DateTime.Now;
-        string hour = LeadingZero(time.Hour);
-        string minute = LeadingZero(time.Minute);
-        string second = LeadingZero(time.Second);
+       if(gameover == false)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        textClock.text = time.ToString(@"hh\:mm\:ss");
 
-        textClock.text = hour + ":" + minute + ":" + second;
+        
     }
 
-    string LeadingZero(int n)
-    {
-        return n.ToString().PadLeft(2, '0');
-    }
+    
 }
