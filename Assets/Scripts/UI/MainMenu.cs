2f4c3883543ce760daba7f77d8a1e5be182e666e
Henry <henryhaak25@gmail.com>
2022-03-04 18:53:06 -0700

    final version (oops)

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
 {
     public string playScene;
     public void startGame(){
+      Debug.Log(playScene);
       SceneManager.LoadScene(playScene);
     }
     public void endGame(){
